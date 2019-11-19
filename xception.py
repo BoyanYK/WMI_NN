07c0a8ac3f039a0f3a66fb6d77f9d9f360540184
Anthony Chua <ossig.anthony@gmail.com>
2019-11-19 23:52:21 +0900

    added prediction loop

--- xception.py
+++ xception.py
@@ -4,6 +4,14 @@ from keras.preprocessing.image import img_to_array
 from keras.applications.vgg16 import preprocess_input
 from keras.applications.vgg16 import decode_predictions
 
+import datetime
+import time
+
+print("BEGIN DRY RUN")
+ts = time.time()
+st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+print(st)
+
 model = Xception(include_top=True, weights='imagenet', input_tensor=None, classes=1000)
 image = load_img('dog.jpg', target_size=(299, 299))
 # convert the image pixels to a numpy array
@@ -23,5 +31,34 @@ label = decode_predictions(yhat)
 # retrieve the most likely result, e.g. highest probability
 label = label[0][0]
 # print the classification
-print('%s (%.2f%%)' % (label[1], label[2]*100))
+print('%s (%.2f%%)' % (label[1], label[2] * 100))
+ts = time.time()
+st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+print(st)
+print("END DRY RUN")
+count = 0
+y = 10
+for x in range(y):
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    print("start run:" + str(x) + " " + st)
+    yhat = model.predict(image)
 
+    # convert the probabilities to class labels
+    label = decode_predictions(yhat)
+    # retrieve the most likely result, e.g. highest probability
+    label = label[0][0]
+    # print the classification
+    print('%s (%.2f%%)' % (label[1], label[2] * 100))
+    ts1 = time.time()
+    st1 = datetime.datetime.fromtimestamp(ts1).strftime('%Y-%m-%d %H:%M:%S')
+    tsA = ts1 - ts
+    print("end run:" + str(x) + " " + st1 + " total seconds: " + str(tsA))
+    print(" ")
+    count = count + tsA
+
+total = count
+count = count / y
+print("total runs: " + str(y))
+print("total time taken: " + str(total))
+print("average time taken: " + str(count))
